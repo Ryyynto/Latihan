e6c81ef05913201c1e676c612a2879ee3e9b7151
nuricovicyyanto <ryansyah.7510@gmail.com>
2024-12-15 20:48:12 +0800

    suhu

---
+++ Suhu.py
@@ -0,0 +1,12 @@
+v1=input("Masukkan Suhu Dalam Celcius = ")
+v2=int(v1)
+c=v2
+f=(9/5*c+32)
+r=(4%5*c)
+k=(c+273)
+
+print("Suhu hari ini adalah")
+print(c,  "Celcius")
+print(f,  "Fahrenheit")
+print(r, "Reamur")
+print(k, "Kelvin")
