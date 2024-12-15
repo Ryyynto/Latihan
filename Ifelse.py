f597b42d4ce3a6ba559781d60764de33e9849fb1
nuricovicyyanto <ryansyah.7510@gmail.com>
2024-12-15 21:04:28 +0800

    Ifelse

---
+++ Ifelse.py
@@ -0,0 +1,17 @@
+v1=10
+
+#Membandingkan
+if v1 > 20 : 
+    print(v1, "Lebih besar dari 20")
+
+elif v1==20 :
+    print(v1, "Sama dengan 20")
+
+elif v1!=20 :
+    print(v1, "Tidak sama dengan 20")
+
+else : 
+    print(v1, "Lebih kecil dari 20")
+
+
+    
