24ab717ec7a7678ddbe9367622912cb97c8470f0
nuricovicyyanto <ryansyah.7510@gmail.com>
2024-12-15 19:06:30 +0800

    Float

---
+++ A.py
@@ -0,0 +1,15 @@
+# Materi Float
+Dec1=3.14
+Dec2=9.81
+
+# Operasi Penjumlahan
+print(Dec1 + Dec2)
+
+# Operasi Pengurangan
+print(Dec1 - Dec2)
+
+# Operasi Perkalian
+print(Dec1 * Dec2)
+
+# Operasi Pembagian
+print(Dec1 / Dec2)
