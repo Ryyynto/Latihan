794dca707397f019bf329d2bb64baac9f58ea0cf
nuricovicyyanto <ryansyah.7510@gmail.com>
2024-12-15 19:25:13 +0800

    data type convertion

---
+++ DTConvertion.py
@@ -0,0 +1,23 @@
+# Variable
+v1="1"
+v2="2"
+
+# Data Type Convertion from string to integer
+v3=int(v1)
+v4=int(v2)
+
+# Data Type Convertion from  string to float
+v5=float(v1)
+v6=float(v2)
+
+# Data Type convertion from string to boolean
+v7=bool(v1)
+
+# Output from string to integer
+print(v3+v4)
+
+# Output from string to float
+print(v5+v6)
+
+#  Output from string to boolean
+print(v7)
