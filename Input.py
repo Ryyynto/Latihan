35c9820b4e9fb77f30eabe5e862cfbb3087cf714
nuricovicyyanto <ryansyah.7510@gmail.com>
2024-12-15 20:32:11 +0800

    input

---
+++ Input.py
@@ -0,0 +1,25 @@
+var1=input("Masukkan angka ")
+var2=input("masukkan angka ")
+v1=int(var1)
+v2=int(var2)
+
+#Input Penjumlahan
+print("Hasil penjumlahan dari", v1, "dan", v2, "adalah", v1 + v2)
+
+#Input Pengurangan
+print("Hasil pengurangan dari", v1, "dan", v2, "adalah", v1 - v2)
+
+#Input Perkalian
+print("Hasil perkalian dari", v1, "dan", v2, "adalah", v1 * v2)
+
+#Input Pembagian
+print("Hasil pembagian dari", v1, "dan", v2, "adalah", v1 / v2)
+
+#Input Pembulatan
+print("Hasil pembulatan dari", v1, "dan", v2, "adalah", v1 // v2)
+
+#Input Perpangkatan
+print("Hasil perpangkatan dari", v1, "dan", v2, "adalah", v1 ** v2)
+
+#Input Sisa Bagi
+print("Hasil sisa bagi dari", v1, "dan", v2, "adalah", v1 % v2)
