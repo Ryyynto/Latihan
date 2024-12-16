f3544700df4f04739404af45d86a9d5541eb58bb
nuricovicyyanto <ryansyah.7510@gmail.com>
2024-12-16 20:26:52 +0800

    Ganti2

---
+++ Kapital.py
@@ -0,0 +1,19 @@
+data=input(" Masukkan Sesuatu ")
+
+#Mengecilkan huruf
+print(data.lower())
+
+#Membesarkan huruf
+print(data.upper())
+
+#Menghitung jumlah huruf tertentu
+print(data.count("a"))
+
+#Menghitung panjang kalimat
+print(len(data))
+
+#Menghitung posisi kata tertentu dari huruf brp
+print(data.find("makan"))
+
+#Mengganti kata
+print(data.replace(" makan", "minum"))
