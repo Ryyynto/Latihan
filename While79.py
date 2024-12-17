9aa5abd766324cc56e7a0ede8c14b063c1967669
nuricovicyyanto <ryansyah.7510@gmail.com>
2024-12-17 19:44:47 +0800

    Create While79.py

---
+++ While79.py
@@ -0,0 +1,21 @@
+a=0
+
+while a<100:
+    a=a+1
+    
+    if a%9==0 :
+        pass
+
+    elif a%7==0 :
+        pass
+
+    elif a<79 :
+        print(a)
+
+    else :
+        break
+
+    
+
+
+        
