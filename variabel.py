6a4d7f00bcfdad12999fbfbdd21196b33219aba9
nuricovicyyanto <ryansyah.7510@gmail.com>
2024-12-18 21:34:22 +0800

    Create variabel.py

---
+++ variabel.py
@@ -0,0 +1,11 @@
+#CamelCase
+pakSusanto = "Saya bapaknya dzakwan"
+
+#SnakeCase
+pak_hendra = "Saya bapaknya rafa"
+
+#PascalCase
+PakNedi = "Saya bapaknya kahfi"
+
+#Variabel constant
+PAK_HILMI = "Saya bapaknya azka"
