90ec7f44c1344a79fe14306cd18e263e40173d11
nuricovicyyanto <ryansyah.7510@gmail.com>
2024-12-19 18:25:33 +0800

    Create Struktur.py

---
+++ Struktur.py
@@ -0,0 +1,25 @@
+#Struktur data set
+var = {1, 2, 3, 4, 5, 6, 7, 8}
+
+print(var)
+
+var.add(9)
+var.add(11)
+
+print(var)
+
+var.remove(3)
+
+print(var)
+
+bis = {1, 2, 3}
+set = {4, 5, 6}
+
+#Union
+print(bis | set)
+
+#Intersection 
+print(var & bis)
+
+#Difference
+print(var - bis)
