cf7f694cdc20072cd068d9eb3de167275c4d9110
nuricovicyyanto <ryansyah.7510@gmail.com>
2024-12-19 18:50:01 +0800

    array

---
+++ Array.py
@@ -0,0 +1,19 @@
+#Array
+buah = ["pisang", "apel", "nanas", "jeruk"]
+
+print(buah)
+
+#Hapus data
+buah.remove("apel")
+
+print(buah)
+
+#Tambah data
+buah.append("Anggur")
+
+print(buah)
+
+#Sortir secara alfabet
+buah.sort()
+
+print(buah)
