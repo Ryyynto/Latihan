d21a3d5b7cdec929211ce1f5f00c141490e88bb6
nuricovicyyanto <ryansyah.7510@gmail.com>
2024-12-19 18:56:00 +0800

    Update settest.py

--- settest.py
+++ settest.py
@@ -1,1 +1,10 @@
-buah = []
+buah = ["apel", "jeruk", "mangga", "pisang"]
+
+print(buah)
+
+tambah = input("Masukkan buah yang ingin ditambahkan ").lower()
+hapus = input("Masukkan buah yang ingin dihapus ").lower()
+buah.append(tambah)
+buah.remove(hapus)
+
+print("Hasil akhir adalah : ", buah)
