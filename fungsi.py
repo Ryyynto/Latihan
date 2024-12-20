2a6205b36997695999fabdeb644835f4f04f04af
nuricovicyyanto <ryansyah.7510@gmail.com>
2024-12-20 15:14:03 +0800

    fungsi

---
+++ fungsi.py
@@ -0,0 +1,15 @@
+def suhu(celcius) : 
+    fahrenheit = 9/5 * celcius + 32
+
+    print(fahrenheit)
+
+def luas(sisi) :
+    persegi  = sisi * sisi
+
+    print(persegi)
+
+def bbi(tinggi) :
+    berat = tinggi - 150
+
+    print(berat)
+    
---
+++ modul.py
@@ -0,0 +1,12 @@
+#memanggil fungsi dari file fungsi
+from fungsi import *
+
+#value fungsi
+suhu(100)
+luas(5)
+bbi(180)
+
+#hasil fungsi
+print(suhu)
+print(luas)
+print(bbi)
